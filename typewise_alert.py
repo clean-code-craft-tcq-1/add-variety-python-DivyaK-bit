45e8dfed0a81809624c4e3d99cd5fa99ebd86a0b
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-30 11:34:10 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,11 +1,7 @@
 
-coolingType_List = {'PASSIVE_COOLING' : {min: 0, max: 35}, 
-                    'HI_ACTIVE_COOLING' : {min: 0, max: 45}, 
-                    'MED_ACTIVE_COOLING' : {min: 0, max: 40}}
+coolingType_List = {'PASSIVE_COOLING' : {min: 0, max: 35}, 'HI_ACTIVE_COOLING' : {min: 0, max: 45}, 'MED_ACTIVE_COOLING' : {min: 0, max: 40}}
 
 breach_Type_Msg = {'TOO_LOW' : 'too low', 'TOO_HIGH' : 'too high', 'NORMAL' : 'normal'}
-
-alert_Target_Type = {"TO_CONTROLLER": send_to_controller, "TO_EMAIL" : send_to_email}
 
 def infer_breach(value, lowerLimit, upperLimit):
   if value < lowerLimit:
@@ -14,27 +10,27 @@ def infer_breach(value, lowerLimit, upperLimit):
     return 'TOO_HIGH'
   return 'NORMAL'
 
-
 def classify_temperature_breach(coolingType, temperatureInC):
+  lowerLimit = 0
+  upperLimit = 0
   if (coolingType in coolingType_List):
     range = coolingType_List[coolingType]
-    return infer_breach(temperatureInC, range['min'], range['max'])
-  else:
-    return infer_breach(temperatureInC, 0, 0)
-
-
-def check_and_alert(alertTarget, batteryChar, temperatureInC):
-  breachType =\
-    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
-  alert_Target_Type[alertTarget](breachType)
-
+    lowerLimit = range['min']
+    upperLimit = range['max']
+  return infer_breach(temperatureInC, lowerLimit, upperLimit)
 
 def send_to_controller(breachType):
   header = 0xfeed
   print(f'{header}, {breachType}')
 
-
 def send_to_email(breachType):
   recepient = "a.b@c.com"
   print(f'To: {recepient}')
   print('Hi, the temperature is ', breach_Type_Msg[breachType])
+
+alert_Target_Type = {"TO_CONTROLLER": send_to_controller, "TO_EMAIL" : send_to_email}
+
+def check_and_alert(alertTarget, batteryChar, temperatureInC):
+  breachType =\
+    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
+  alert_Target_Type[alertTarget](breachType)
