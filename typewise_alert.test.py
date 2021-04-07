f4f1e4065c54c37e56868863ec83b329318fe79d
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-04-07 09:32:24 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -5,7 +5,19 @@ import typewise_alert
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
-
+    self.assertTrue(typewise_alert.infer_breach(100, 0, 45) == 'TOO_HIGH')
+    self.assertTrue(typewise_alert.infer_breach(25, 0, 40) == 'NORMAL')
+    
+ def test_checks_input_data_valid(self):
+  self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', 80))
+  self.assertFalse(typewise_alert.classify_temperature_breach(None, 40))
+  self.assertFalse(typewise_alert.classify_temperature_breach('HI_ACTIVE_COOLING', None))
+  self.assertFalse(typewise_alert.classify_temperature_breach('COOLING', 80))
+        
+ def test_yields_alert_for_breaches(self):
+  self.assertEqual(typewise_alert.check_and_alert('TO_CONTROLLER', {'coolingType': 'PASSIVE_COOLING'}, 120), True)
+  self.assertEqual(typewise_alert.check_and_alert('TO_EMAIL', {'coolingType': 'HI_ACTIVE_COOLING'}, -20), True)
+  self.assertEqual(typewise_alert.check_and_alert('TO_CONSOLE', {'coolingType': 'MED_ACTIVE_COOLING'}, 90), True)
 
 if __name__ == '__main__':
   unittest.main()
